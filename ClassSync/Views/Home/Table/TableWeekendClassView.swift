5ababc24d7012ce6a6e4e66fbb6b23e3c6ea749a
s0-sh1r0 <sou1015ushi@gmail.com>
2025-04-20 23:23:02 +0900

    ConfilmButtonコンポーネント作成

---
+++ ClassSync/Views/Home/Table/TableWeekendClassView.swift
@@ -0,0 +1,18 @@
+//
+//  TableWeekendClassView.swift
+//  ClassSync
+//
+//  Created by 牛田壮志郎 on 2025/04/20.
+//
+
+import SwiftUI
+
+struct TableWeekendClassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableWeekendClassView()
+}

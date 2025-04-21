18fa32b63fa26db3f53a907d2f7fa839ba81a4ad
s0-sh1r0 <sou1015ushi@gmail.com>
2025-04-21 09:22:06 +0900

    TableWeekendClassViewの修正

--- ClassSync/Views/Home/Table/TableSetting/TableWeekendClassView.swift
+++ ClassSync/Views/Home/Table/TableSetting/TableWeekendClassView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct TableWeekendClassView: View {
     @Environment(\.dismiss) var dismiss
     
+    @State private var isSaturdayChecked = false
+    @State private var isSundayChecked = false
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -23,6 +26,31 @@ struct TableWeekendClassView: View {
                         Text("土日の授業")
                             .font(.title2)
                     }
+                    
+                    HStack(spacing: 15) {
+                        Text("土曜日")
+                        Image(systemName: isSaturdayChecked ? "checkmark.square" : "square")
+                            .onTapGesture {
+                                isSaturdayChecked.toggle()
+                            }
+
+                        Text("/")
+
+                        Text("日曜日")
+                        Image(systemName: isSundayChecked ? "checkmark.square" : "square")
+                            .onTapGesture {
+                                // 日曜をONにしたら土曜もONにする
+                                if !isSundayChecked {
+                                    isSundayChecked = true
+                                    isSaturdayChecked = true
+                                } else {
+                                    isSundayChecked = false
+                                }
+                            }
+                    }
+                    .font(.title3)
+                    .padding(.top, 10)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                     
                     Spacer()
                     
